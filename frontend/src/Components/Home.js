76f776b630c0ee132eb23eb3475e94ddccf27228
Prince <princejaj67@gmail.com>
2024-04-24 14:04:30 +0530

    Improved Home page

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -2,14 +2,12 @@ function Home() {
 
   return (
       /* a.first.name means that first is imported as a state component named as first and its name and department value are read*/
-      <div className='mb-4 container'>
-      <details>
+      <div className='mb-4 container text-center'>
+      <h1 className="h1"><b>HOME</b></h1>
       <h2 className="h2">TRY this application for CRUD(Create,Read,Update,Delete) operations on simple notes </h2>
       <h2 className="h2">Served with authentication from bcryptjs, authToken and others it is as secure as it is simple</h2>
       <h2 className="h2">Built for all types of devices, and fully responsive we will always try to improve this.</h2>
       <h2 className="h2">STAY Connected with US.</h2>
-      <summary className="fs-1">HOME</summary>
-      </details>
       
     </div>
   )
